f0f92f87fcb748886ef187b3280dc702e53121e1
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2024-01-23 14:13:56 +0200

    Add files via upload

--- Sales_analysis.sql
+++ Sales_analysis.sql
@@ -1,7 +1,7 @@
 #Sales analysis
 
 #Total Sales by year
-SELECT YEAR(o.order_date), 
+SELECT YEAR(o.order_date) AS year, 
 ROUND(SUM(oi.quantity * (oi.list_price - (oi.list_price * oi.discount))),2) as total_Sales 
 from order_items oi 
 JOIN orders o ON oi.order_id = o.order_id 
@@ -28,20 +28,78 @@ JOIN orders o ON oi.order_id = o.order_id
 GROUP BY YEAR(order_date), MONTH(order_date)
 ORDER BY YEAR(order_date), MONTH(order_date)
 
-#Total quantity of sold bikes grouped by discount
-SELECT oi.discount,
-sum(oi.quantity) AS sold_bikes
-from order_items oi 
-GROUP BY oi.discount
-ORDER BY oi.discount DESC 
+_________________________________________________________________________________
+
 
 #Query identify products frequently purchased together
-SELECT oi1.product_id as product1, oi2.product_id as product2, COUNT(*) as occurrences
+SELECT oi1.product_id as product1, p1.product_name, oi2.product_id as product2, p2.product_name, COUNT(*) as occurrences
 FROM order_items oi1
 JOIN order_items oi2 ON oi1.order_id = oi2.order_id AND oi1.product_id > oi2.product_id
+JOIN products p1 ON p1.product_id = oi1.product_id 
+JOIN products p2 ON p2.product_id = oi2.product_id 
 GROUP BY product1, product2
 -- HAVING occurrences >= 10 -- set your minimum occurrences threshold
 ORDER BY occurrences DESC;
 
+_________________________________________________________________________________
 
 
+#query to calculate the total sales for each bike brand.
+SELECT b.brand_name, round(SUM(oi.quantity * (oi.list_price - (oi.list_price * oi.discount))),2) as total_sales
+FROM order_items oi 
+JOIN products p on oi.product_id = p.product_id  
+JOIN brands b on p.brand_id = b.brand_id 
+GROUP by b.brand_name
+ORDER BY  total_sales DESC
+
+#the bike products with the highest total sales.
+select p.product_name, b.brand_name, ROUND(SUM(oi.quantity * (oi.list_price - (oi.list_price * oi.discount))), 2) as total_sales
+from order_items oi 
+join products p on oi.product_id = p.product_id  
+join brands b on p.brand_id = b.brand_id 
+group by p.product_name, b.brand_name
+order by total_sales desc
+limit 10
+
+#query to calculate the total sales for each bike category.
+select c.category_name , ROUND(SUM(oi.quantity * (oi.list_price - (oi.list_price * oi.discount))), 2) as total_sales
+from order_items oi 
+join products p on oi.product_id = p.product_id  
+join categories c on p.category_id = c.category_id 
+group by c.category_name
+order by total_sales DESC
+
+#query provides information on the average 
+#price of products for each combination of 
+#category and brand, excluding combinations where there are no matching products
+SELECT c.category_name, b.brand_name, round(avg(p.list_price),2) as avg_price
+FROM categories c
+CROSS JOIN brands b
+LEFT JOIN products p ON b.brand_id = p.brand_id AND c.category_id = p.category_id
+GROUP by c.category_name, b.brand_name
+HAVING COUNT(p.product_id) > 0
+ORDER BY  avg_price DESC
+
+_________________________________________________________________________________
+
+
+#Total spending by customer
+select c.first_name, c.last_name, SUM(oi.quantity * (oi.list_price - (oi.list_price * oi.discount))) as total_Sales from customers c 
+join orders o ON c.customer_id = o.customer_id 
+join order_items oi ON o.order_id = oi.order_id
+group by c.first_name, c.last_name
+order by total_Sales desc 
+
+#Total spending by customers in each state:
+select c.state, SUM(oi.quantity * (oi.list_price - (oi.list_price * oi.discount))) as total_Sales from customers c 
+join orders o ON c.customer_id = o.customer_id 
+join order_items oi ON o.order_id = oi.order_id
+group by c.state
+order by total_Sales desc 
+
+#Total spending by customers in each city:
+select c.state, c.city , SUM(oi.quantity * (oi.list_price - (oi.list_price * oi.discount))) as total_Sales from customers c 
+join orders o ON c.customer_id = o.customer_id 
+join order_items oi ON o.order_id = oi.order_id
+group by c.state, c.city
+order by total_Sales desc 
